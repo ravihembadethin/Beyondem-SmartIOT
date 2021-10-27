2fef532e9abdf3b9997dcb671036e89afa03b613
avinashchavanfidel <avinash.chavan.fidel@gmail.com>
2021-10-27 19:35:27 +0530

    Commit27Oct

--- route/routes.js
+++ route/routes.js
@@ -1,107 +1,101 @@
-const express=require('express')
+const express=require('express');
+const { database } = require('faker');
 const mongoose=require('mongoose')
 
 const router=express.Router();
 
 const Schema=mongoose.Schema
-
-const testcoll=new Schema({},{strict:false})
-const testCollection=mongoose.model('test',testcoll)
+const sensorSchema=new Schema({},{strict:false,versionKey: false})
+const sensorcollection=mongoose.model('sensor',sensorSchema)
+const sensordatacollection=mongoose.model('sensorData',sensorSchema)
 
 router.post('/postman-echo.com/post',async (req,res)=>{
 
-    const body={...req.body}
-    body.date=new Date();
-
-    await  testCollection.findOne({name: req.body.name},
-        (err,response)=>{
-                if(response!=null)
-                {
-                console.log(' found')
-                body.deviceID=response.deviceID;
-
-                const testdata=new testCollection(body) 
-                testdata.save();
-                res.status(200).send({ "message": "Data Updated" });
+    await  sensorcollection.findOne({name: req.body.name},
+            (err,response)=>{
+                if(response!=null){
+                    console.log('responce',response);
+                    if(response.type.indexOf(req.body.type) == -1)
+                    {
+                    sensorcollection.updateOne(
+                            { name: response.name },
+                            { 
+                                $push: { type: req.body.type }
+                            },
+                            async (err, response) => {
+                            if(err) res.status(404).send({ "error": err });
+                            }
+                        )
+                    }
+                    
+                    storedata(response.deviceID,req.body);
+                    res.status(200).send({ "message": "Data Updated" });
+                
                 }
-                else {
-                if (body.deviceID ==null){
-                    body.deviceID = generateRandomId();
+                else{
+                    sensor={
+                        deviceID:req.body.deviceID==null || req.body.deviceID=='' ?generateRandomId():Number(req.body.deviceID),
+                        name:req.body.name,
+                        type:[req.body.type]
+                    }
+                    new sensorcollection(sensor).save();
+                    storedata(sensor.deviceID,req.body);
+                    res.status(200).send({ "message": "New Sensor Data Added" , deviceID: sensor.deviceID});
+
                 }
-                const testdata=new testCollection(body) 
-                testdata.save();
-                res.status(200).send({ "message": "New Device Connected" ,"deviceID":body.deviceID});
-                }    
         }).clone().catch(function(err){ console.log(err)})
 })
 
 
 router.get('/sensor',async(req,res)=>{
-
-   await testCollection.find({},((err,result)=>
+   await sensordatacollection.find({},{ _id: 0 ,Date:0},((err,result)=>
    {
         if(err) throw err;
-        res.status(200).send({'data':result})
+        res.status(200).send(result)
    }) ).clone().catch(function(err){ console.log(err)})
 })
 
 router.get('/sensor/:sensorid',async(req,res)=>{
-    if(isNaN(req.params.sensorid)){
-        await testCollection.find({name:req.params.sensorid}, (err,result)=>{
-            if(err) throw err;   
+   await sensordatacollection.find({deviceID:Number(req.params.sensorid)},{ _id: 0 ,Date:0,deviceID:0}, (err,result)=>{
+            if(err) throw err;
+            if(result.length>0)   
           res.status(200).send(result);
         }).clone().catch(function(err){ console.log(err)})
-    }
-    else{
-        await testCollection.find({deviceID:Number(req.params.sensorid)}, (err,result)=>{
-            if(err) throw err;   
-          res.status(200).send(result);
-        }).clone().catch(function(err){ console.log(err)})
-    }
-  
 })
 
 router.get('/sensors/:sensorid',async(req,res)=>{
     var startDate= new Date(req.body.startDate);
     var endDate=new Date((req.body.endDate)+ 'T23:59:59');
-
-    if(isNaN(req.params.sensorid)){
-        await testCollection.find({name:req.params.sensorid,date: { $lte: endDate , $gte:startDate }}, (err,result)=>{
-            if(err) throw err;   
-            res.status(200).send(result);
-        }).clone().catch(function(err){ console.log(err)})
-    }
-    else{
-        await testCollection.find({deviceID:Number(req.params.sensorid) ,date: { $lte: endDate , $gte:startDate }}, (err,result)=>{
-            if(err) throw err;   
-            res.status(200).send(result);
-        }).clone().catch(function(err){ console.log(err)})
-    }
+    await sensordatacollection.find({deviceID:Number(req.params.sensorid) ,Date: { $lte: endDate , $gte:startDate }},{ _id: 0 ,Date:0,deviceID:0}, (err,result)=>{
+        if(err) throw err;
+        res.status(200).send(result);
+    }).clone().catch(function(err){ console.log(err)})
   
-})
-
+}) 
 router.get('/sensor/:sensorid/:type',async(req,res)=>{
    
    var startDate= new Date(req.body.startDate);
     var endDate=new Date((req.body.endDate)+ 'T23:59:59'); 
-  
-    if(isNaN(req.params.sensorid)){
-        await testCollection.find({name:req.params.sensorid,type:req.params.type ,date: { $lte: endDate , $gte:startDate }}, (err,result)=>{
-            if(err) throw err;   
-            res.status(200).send(result);
+        await sensordatacollection.find({deviceID:Number(req.params.sensorid),type:req.params.type ,Date:{$lte: endDate , $gte:startDate }},{ _id: 0 ,Date:0,deviceID:0,type:0}, (err,result)=>{
+            if(err) throw err; 
+            const data=[];
+              result.forEach(element => {
+                  var d=JSON.stringify(element)
+                  var x=JSON.parse(d)
+                  data.push(x.data)
+              });
+            res.status(200).send(data);
         }).clone().catch(function(err){ console.log(err)})
-    }
-    else
-    {
-        await testCollection.find({deviceID:Number(req.params.sensorid),type:req.params.type ,date: { $lte: endDate , $gte:startDate }}, (err,result)=>{
-            if(err) throw err;   
-            res.status(200).send(result);
-        }).clone().catch(function(err){ console.log(err)})
-    }
 })
 
 function generateRandomId(){
  return Math.floor(1000 + Math.random() * 9000);
 }
 
+async function storedata(ID,req){
+    const {name, ...filteredObject} = req;
+    filteredObject.Date=new Date();
+    filteredObject.deviceID=ID;
+   await new sensordatacollection(filteredObject).save()
+}
 module.exports = router;
